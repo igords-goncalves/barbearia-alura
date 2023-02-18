a08fce26c0c79b48c39a187cf527158b044fae3d
Igor Gonçalves <igordossantos38@gmail.com>
2023-02-18 18:01:43 -0300

    feat: import function from other module

--- src/js/validityForm.js
+++ src/js/validityForm.js
@@ -1,51 +1,20 @@
+import {
+    isInputValid,
+    showError,
+    handleSubmit,
+} from "./utils/validityFunctions.js";
+
 const form = document.querySelector("form");
-const inputEmail = document.getElementById("email");
-const emailError = document.querySelector("#email + span.error");
 const buttonForm = document.querySelector(".button-form");
 
-/**
- * Check if input is validity returning true or false
- * 
- * @param {HTMLElement | null} input - Represent inputs captured
- * @param {Element | null} error - Represent error element
- * @param {Element | null} button - Represent button that submits
- * @param {Function} callBack
- * @returns {void} - It would return undefined with a console.log
- * 
- * Trying to work with a pure function
- */
-function isInputValid(input, error, button, callBack) {
-    input.addEventListener("input", () => {
-        if (input.validity.valid) {
-            error.textContent = "";
-            error.className = "error";
-            button.removeAttribute("disabled", "");
-        } else {
-            callBack(input, error);
-            button.setAttribute("disabled", "");
-        }
-    });
-}
+const inputName = document.getElementById("name")
+const nameError = document.getElementById("#name span.error")
+
+const inputEmail = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+const inputTel = document.getElementById("tel");
+const telError = document.querySelector("#tel + span.error");
+
 isInputValid(inputEmail, emailError, buttonForm, showError);
-
-
-
-
-form.addEventListener("submit", (event) => {
-    if (!emailError.validity.valid) {
-        showError(inputEmail, emailError);
-        event.preventDefault();
-    }
-});
-
-function showError(input, error) {
-    if (input.validity.valueMissing) {
-        error.textContent = "Inserir um endereço de e-mail";
-    } else if (input.validity.typeMismatch) {
-        error.textContent = "Inserir um e-mail válido";
-    } else if (input.validity.tooShort) {
-        error.textContent = `O email não pode ter menos que ${input.minLength} caracteres`;
-    }
-
-    error.className = "error active";
-}
+handleSubmit(form, emailError, showError);
